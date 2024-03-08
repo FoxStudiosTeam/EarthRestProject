dc12798e503a3953c20a6621431545c9fa83316c
xxlegendzxx22 <alberto336@mail.ru>
2024-03-08 15:49:39 +0500

    Zemlya123

---
+++ src/main/kotlin/ru/foxstudios/earthrestproject/EarthRestProjectApplication.kt
@@ -0,0 +1,11 @@
+package ru.foxstudios.earthrestproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EarthRestProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<EarthRestProjectApplication>(*args)
+}
---
+++ src/test/kotlin/ru/foxstudios/earthrestproject/EarthRestProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.foxstudios.earthrestproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EarthRestProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
