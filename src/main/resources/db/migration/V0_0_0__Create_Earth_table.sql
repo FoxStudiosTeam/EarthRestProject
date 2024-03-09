2bae1f4cf3f712201c0b997246de19b2836a1851
xxlegendzxx22 <alberto336@mail.ru>
2024-03-10 04:08:07 +0500

    Commit_EarthRestProject migracya2

--- src/main/resources/db/migration/V0_0_0__Create_Earth_table.sql
+++ src/main/resources/db/migration/V0_0_0__Create_Earth_table.sql
@@ -1,10 +1,10 @@
 create table earth_table
 (
-    uuid varchar
+    uuid varchar(64)
         constraint table_name_pk
             primary key,
-    name varchar,
-    commentary varchar,
-    file varchar,
-    ext varchar,
+    name varchar(128),
+    commentary text,
+    file text,
+    ext varchar(128),
 )
